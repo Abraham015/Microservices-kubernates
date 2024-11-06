d4218790168535cdf3202794bc87c439115caa32
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2024-11-06 10:42:21 -0600

    Finishing endpoints

--- services/employee_service/src/main/java/dev/abraham/employee_service/controller/EmployeeController.java
+++ services/employee_service/src/main/java/dev/abraham/employee_service/controller/EmployeeController.java
@@ -6,10 +6,12 @@ import dev.abraham.employee_service.service.EmployeeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +22,25 @@ public class EmployeeController {
     @PostMapping("/create")
     public ResponseEntity<EmployeeResponse> createEmployee(@RequestBody @Valid EmployeeRequest employeeRequest){
         return ResponseEntity.ok(employeeService.createEmployee(employeeRequest));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<EmployeeResponse>> getAllEmployees(){
+        return ResponseEntity.ok(employeeService.getAll());
+    }
+
+    @GetMapping("/{employee_id}")
+    public ResponseEntity<EmployeeResponse> getEmployeeById(@PathVariable Integer employee_id){
+        try {
+            return ResponseEntity.ok(employeeService.getEmployeeById(employee_id));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(null);
+        }
+    }
+
+    @DeleteMapping("/{employee_id}")
+    public ResponseEntity<Void> deleteEmployeeById(@PathVariable Integer employee_id){
+        employeeService.deleteEmployeeById(employee_id);
+        return ResponseEntity.accepted().build();
     }
 }
--- services/employee_service/src/main/java/dev/abraham/employee_service/service/EmployeeService.java
+++ services/employee_service/src/main/java/dev/abraham/employee_service/service/EmployeeService.java
@@ -5,8 +5,11 @@ import dev.abraham.employee_service.model.Employee;
 import dev.abraham.employee_service.repository.EmployeeRepository;
 import dev.abraham.employee_service.request.EmployeeRequest;
 import dev.abraham.employee_service.response.EmployeeResponse;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,22 @@ public class EmployeeService {
         Employee employee = employeeMapper.toEmployee(employeeRequest);
         Employee savedEmployee=employeeRepository.save(employee);
         return employeeMapper.toEmployeeResponse(savedEmployee);
+    }
+
+    public List<EmployeeResponse> getAll() {
+        return employeeRepository.findAll()
+                .stream()
+                .map(employeeMapper::toEmployeeResponse)
+                .toList();
+    }
+
+    public EmployeeResponse getEmployeeById(Integer employeeId) {
+        return employeeRepository.findById(employeeId)
+                .map(employeeMapper::toEmployeeResponse)
+                .orElseThrow(()->new EntityNotFoundException("Employee does not exist"));
+    }
+
+    public void deleteEmployeeById(Integer employeeId) {
+        employeeRepository.deleteById(employeeId);
     }
 }
